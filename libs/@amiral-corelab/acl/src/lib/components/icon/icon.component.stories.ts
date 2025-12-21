3d4dba792f4ca2c4bf4696f31c569ac269a5ee4b
Alexandre GALLAIS <37043327+AlexandreGallais@users.noreply.github.com>
2025-12-21 14:01:12 +0100

    🎨 Update icon component

--- libs/@amiral-corelab/acl/src/lib/components/icon/icon.component.stories.ts
+++ libs/@amiral-corelab/acl/src/lib/components/icon/icon.component.stories.ts
@@ -9,27 +9,12 @@ const meta: Meta<IconComponent> = {
   tags: ['autodocs'],
   argTypes: {
     name: {
-      type: { name: 'string', required: true },
       control: { type: 'select' },
       options: iconConstant,
-      table: {
-        category: 'Inputs',
-        type: {
-          summary: 'Icons',
-          detail: iconConstant.map((x) => `"${x}"`).join(' | '),
-        },
-      },
     },
   },
   args: {
     name: 'brand-acl',
-  },
-  parameters: {
-    docs: {
-      description: {
-        component: 'Setting `color` in your CSS will change the icon color.',
-      },
-    },
   },
 };
 
