989aa6bce051e27cbb49bfb19004dcdb0d5769c7
Alexandre GALLAIS <37043327+AlexandreGallais@users.noreply.github.com>
2025-12-21 14:57:58 +0100

    ✨ Add input checkbox component

---
+++ libs/@amiral-corelab/acl/src/lib/components/input-checkbox/input-checkbox.component.spec.ts
@@ -0,0 +1,28 @@
+import type { ComponentFixture } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { InputCheckboxComponent } from './input-checkbox.component';
+import { beforeEach, describe, expect, it } from 'vitest';
+
+describe('InputRadioComponent', () => {
+  let component: InputCheckboxComponent;
+  let fixture: ComponentFixture<InputCheckboxComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [InputCheckboxComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(InputCheckboxComponent);
+    fixture.componentRef.setInput('name', 'name');
+    fixture.componentRef.setInput('indeterminate', 'false');
+    fixture.componentRef.setInput('model', 'false');
+    component = fixture.componentInstance;
+
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
