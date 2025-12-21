dbf282aef17824cc5626b43c8fef443ba155beb1
Alexandre GALLAIS <37043327+AlexandreGallais@users.noreply.github.com>
2025-12-21 14:52:38 +0100

    🎨 Enhance input radio component

--- libs/@amiral-corelab/acl/src/lib/components/icon/icon.constant.ts
+++ libs/@amiral-corelab/acl/src/lib/components/icon/icon.constant.ts
@@ -7,5 +7,6 @@ export const iconConstant = [
   'brand-acl',
   'input-check-mark',
   'input-indeterminate-mark',
+  'input-radio-mark',
   'shape-circle',
 ];
--- libs/@amiral-corelab/acl/src/lib/components/input-radio/input-radio.component.ts
+++ libs/@amiral-corelab/acl/src/lib/components/input-radio/input-radio.component.ts
@@ -1,11 +1,39 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, model, ViewEncapsulation } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'acl-input-radio',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './input-radio.component.html',
   styleUrl: './input-radio.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class InputRadioComponent {}
+export class InputRadioComponent {
+  /**
+   * @remarks
+   * **@required**
+   *
+   * @description
+   * Input name
+   */
+  public readonly name = input.required<string>();
+
+  /**
+   * @remarks
+   * **@required**
+   *
+   * @description
+   * Input value
+   */
+  public readonly value = input.required<string>();
+
+  /**
+   * @remarks
+   * **@required**
+   *
+   * @description
+   * Input model
+   */
+  public readonly model = model.required<string>();
+}
